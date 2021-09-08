e4b68ed6e17688defa3abea1d66cec0730526bfb
danio24 <danio.quero@gmail.com>
2021-09-08 16:01:36 +0200

    walking/idle animation transitions

--- Assets/The Great Fleece/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/The Great Fleece/Game/Scripts/Player/PlayerMovement.cs
@@ -8,8 +8,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private NavMeshAgent _agent;
 
+    [SerializeField]
+    private Animator _animator;
+    private void Update()
+    {
+        _animator.SetBool("isWalking", isWalking());
+    }
+
     public void Move(Vector3 pos)
     {
         _agent.destination = pos;
+    }
+
+    private bool isWalking()
+    {
+        return _agent.velocity != Vector3.zero;
     }
 }
