2a48e3302773953f81b135ca0e78c49b764e4875
danio24 <danio.quero@gmail.com>
2021-09-10 11:48:26 +0200

    patrol timing optimization

--- Assets/The Great Fleece/Game/Scripts/Enemy/EnemyAI.cs
+++ Assets/The Great Fleece/Game/Scripts/Enemy/EnemyAI.cs
@@ -6,37 +6,54 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> waypoints;
+    public bool isPatrolling;
     private int _currentTargetId = -1;
     private int _direction = 1;
     [SerializeField]
     private NavMeshAgent _agent;
 
+    private WaitForSeconds _idleOnTarget;
+    private WaitForSeconds _patroStep;
+
     void Start()
     {
+        _idleOnTarget = new WaitForSeconds(1f);
+        _patroStep = new WaitForSeconds(0.2f);
+
         if (waypoints != null && waypoints.Count > 0)
         {
             _currentTargetId = 0;
             _agent.destination = waypoints[_currentTargetId].position;
-        }           
-    }
+        }
 
-    void Update()
-    {
-        if (waypoints != null && waypoints.Count > 0)
-            if (transform.position == waypoints[_currentTargetId].position)
-            {
-                _agent.destination = waypoints[NextTarget()].position;
-            }
+        StartCoroutine(PatrolCoroutine());
     }
-
     private int NextTarget()
     {
-            int next = _currentTargetId + _direction;
-            if (next == waypoints.Count || next == -1)
-                _direction *= -1;
+        int next = _currentTargetId + _direction;
+        if (next == waypoints.Count || next == -1)
+            _direction *= -1;
 
-            _currentTargetId += _direction;
+        _currentTargetId += _direction;
 
-            return _currentTargetId;
+        return _currentTargetId;
+    }
+
+    private IEnumerator PatrolCoroutine()
+    {
+        while (isPatrolling)
+        {
+            if (waypoints != null && waypoints.Count > 0)
+                if (transform.position == waypoints[_currentTargetId].position)
+                {
+                    if (_currentTargetId == 0 || _currentTargetId == waypoints.Count - 1)
+                        yield return _idleOnTarget;
+
+                    _agent.destination = waypoints[NextTarget()].position;
+                }
+
+                else
+                    yield return _patroStep;
+        }
     }
 }
