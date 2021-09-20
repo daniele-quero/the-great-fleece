4525f5ed54d3b9ebb6a776237baec46af277f204
danio24 <danio.quero@gmail.com>
2021-09-21 01:03:03 +0200

    uimanager - cutscene menu buttons

---
+++ Assets/The Great Fleece/Game/Scripts/Canvas/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("Missing UIManager");
+
+            return _instance;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
